709cec8b5afb907b7f52ab9c3ec58a3aeb5c9cc3
Greg Bowler <greg@g105b.com>
2026-03-01 21:34:59 +0000

    tweak: attempt 8.4/8.5 compatibility

--- src/HTMLDocument.php
+++ src/HTMLDocument.php
@@ -36,15 +36,8 @@ class HTMLDocument extends Document {
 		$this->loadHTML($html, LIBXML_SCHEMA_CREATE | LIBXML_COMPACT);
 		foreach($this->childNodes as $child) {
 // For the workaround noted above, the functionality changes slightly after PHP 8.5.
-			if(version_compare(PHP_VERSION, "8.5") >= 0) {
-				if($child->nodeValue === "?xml encoding=\"utf-8\" ?") {
-					$this->removeChild($child);
-				}
-			}
-			else {
-				if($child instanceof ProcessingInstruction) {
-					$this->removeChild($child);
-				}
+			if($child->nodeValue === "?xml encoding=\"utf-8\" ?") {
+				$this->removeChild($child);
 			}
 		}
 
